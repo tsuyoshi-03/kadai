6911aeecd8855d33050a7eebe26955e0b8f5f3d1
tsuyoshi <zynb0316@icloud.com>
2021-03-10 02:00:17 +0000

    pictgram0310

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true,length:{minimum:0,maxmum:15}  
   validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password,presence: true,length: {minimum:8,maxmum:32},format: { with:/\A(?=.*?[a-z])[a-z\d]{8,32}+\z/ }
+  validates :password,presence: true,length: {minimum:8,maxmum:32},format: { with:/\A(?=.*?[a-z])[a-z\d]+\z/ }
   has_secure_password
 end
--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
   root 'pages#index'
   get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.ht
   get 'pages/link'
+  
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
