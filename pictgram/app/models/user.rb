6cd3f358659c2c43a9d9b2714b589d5138f8f96b
tsuyoshi <zynb0316@icloud.com>
2021-03-16 08:48:20 +0000

    0316

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true,length:{minimum:0,maxmum:15}  
+  validates :name, presence: true, length: { maximum: 15 }  
   validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password,presence: true,length: {minimum:8,maxmum:32},format: { with:/\A(?=.*?[a-z])[a-z\d]+\z/ }
+  validates :password,presence: true,length: { in: 8..32 },format: { with:/\A(?=.*?[a-z])[a-z\d]+\z/ }
+  
   has_secure_password
+
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  
+
   root 'pages#index'
   get 'pages/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.ht
-  get 'pages/link'
-  
-  resources :users
-  
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
